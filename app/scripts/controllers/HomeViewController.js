d0d1bf7c672d2b1a78ab8425fe9eb53e417f47a8
Ana Trakhtman <alkadur@gmail.com>
2015-01-26 12:07:17 +0200

    Wrap the controller in js module pattern

--- app/scripts/controllers/HomeViewController.js
+++ app/scripts/controllers/HomeViewController.js
@@ -1,22 +1,24 @@
-define(['app'], function (app) {
-  'use strict';
+(function (define) {
+    "use strict";
 
-  /**
-   * @ngdoc function
-   * @name HomeViewController
-   * @description
-   * # MainCtrl
-   * Controller of the angularAmdApp
-   */
-  app.controller('HomeViewController',['$scope', '$log', function ($scope, $log) {
-      $log = $log.getInstance('HomeViewController');
-      $log.debug('init');
+    define(['app'], function (app) {
+        /**
+         * @ngdoc function
+         * @name HomeViewController
+         * @description
+         * # MainCtrl
+         * Controller of the angularAmdApp
+         */
+        app.controller('HomeViewController', ['$scope', '$log', function ($scope, $log) {
+            $log = $log.getInstance('HomeViewController');
+            $log.debug('init');
 
-      $scope.app = 'AngularAmdApp';
-      $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-      ];
-    }]);
-});
+            $scope.app = 'AngularAmdApp';
+            $scope.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+        }]);
+    });
+}(define));
